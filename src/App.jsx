e93ef1ac2efc9d5e8cb59c2f76e427c29d381e6d
Folarin Obajenihi <folarinobajenihi@gmail.com>
2026-02-10 15:53:12 +0100

    Import App.css for styling in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home';
